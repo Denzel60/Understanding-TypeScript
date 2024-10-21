428526e7191babd02b9904c90345ac8819c8dbf5
Denzel <lawrencedenzel60@gmail.com>
2024-10-21 21:31:13 +0300

    "Setting up the project, installing dependencies and Added rest parameter to function and updated script tag in index.html"

--- TypeScript Compiler/dist/app.js
+++ TypeScript Compiler/dist/app.js
@@ -1,2 +1,6 @@
 "use strict";
 console.log("Hello");
+const arr = (a, b, ...c) => {
+    console.log(a, b, c);
+};
+arr(34, 'str', 222223, 23332, 23333);
--- TypeScript Compiler/src/app.ts
+++ TypeScript Compiler/src/app.ts
@@ -1,1 +1,7 @@
 console.log("Hello");
+
+const arr = (a:number , b: string, ...c:any) => {
+    console.log(a, b, c);
+}
+
+arr (34, 'str', 222223, 23332, 23333)
