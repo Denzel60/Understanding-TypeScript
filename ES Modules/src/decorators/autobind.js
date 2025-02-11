c9b788cfb3c559bb9bd584e02f8da1803c5cb24a
Denzel <lawrencedenzel60@gmail.com>
2025-02-11 14:08:42 +0300

    Added on using ES Modules in the Drag and Drop project

---
+++ ES Modules/src/decorators/autobind.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.autobind = autobind;
+// autobind decorator
+function autobind(_, _2, descriptor) {
+    var originalMethod = descriptor.value;
+    var adjDescriptor = {
+        configurable: true,
+        get: function () {
+            var boundFn = originalMethod.bind(this);
+            return boundFn;
+        }
+    };
+    return adjDescriptor;
+}
